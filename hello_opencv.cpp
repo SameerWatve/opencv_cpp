0756a3139983575b1a41ebba5249cc01e579c2f0
Sameer Watve <swatve@imagesensing.com>
2021-10-05 20:41:31 -0500

    first commit

---
+++ hello_opencv.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    cv::Mat image;
+    image = imread( argv[1], 1 );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    image = image / 2;
+    cv::imwrite("./2.png",image);
+    return 0;
+}
